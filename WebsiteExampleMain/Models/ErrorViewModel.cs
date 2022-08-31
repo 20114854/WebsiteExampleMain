ba6b1efe64367bee2819dfecdbadf751af10f938
Keagan Herman <Hermanleah75@gmail.com>
2022-08-31 09:09:32 +0200

    Add project files.

---
+++ WebsiteExampleMain/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebsiteExampleMain.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebsiteExampleMain/Models/Staff.cs
@@ -0,0 +1,31 @@
+﻿namespace WebsiteExampleMain.Models
+{
+    public class Staff
+    {
+        int id;
+        string title;
+        string name;
+        string password;
+        string dob;
+
+        public Staff()
+        {
+
+        }
+
+        public Staff(int id, string title, string name, string password, string dob)
+        {
+            this.id = id;
+            this.title = title;
+            this.name = name;
+            this.password = password;
+            this.dob = dob;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Title { get => title; set => title = value; }
+        public string Name { get => name; set => name = value; }
+        public string Password { get => password; set => password = value; }
+        public string Dob { get => dob; set => dob = value; }
+    }
+}
