fa0bf68c0f82153fd2f94babb1bf3af63ef5e5b4
Keagan Herman <Hermanleah75@gmail.com>
2022-08-31 11:49:19 +0200

    Updates

---
+++ WebsiteExampleMain/Controllers/StaffContoller.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WebsiteExampleMain.Models;
+
+namespace WebsiteExampleMain.Controllers
+{
+
+
+    public class StaffContoller : Controller
+    {
+
+        StaffContext db = new StaffContext();
+        public IActionResult StaffList()
+        {
+            return View(StaffContext.Staffobject);
+        }
+
+        public IActionResult AddStaff()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult AddStaff(Staff sf)
+        {
+            Staff data = sf;
+            if(sf.Name==null||sf.Id<0||sf.Password==null||sf.Dob==null||sf.Title==null)
+            {
+                ViewBag.Error = "Please enter all the values in each field";
+                return View();
+            }
+            else
+            {
+                StaffContext.Staffobject.Add(sf);
+                return RedirectToAction("StaffList");
+            }
+
+        }
+
+        public IActionResult EditStaff(int id)
+        {
+            Staff temp = StaffContext.Staffobject.Where(x => x.Id==id).FirstOrDefault();
+            return View(temp);
+        }
+
+        public IActionResult EditStaff(Staff sf)
+        {
+            Staff data = sf;
+            if (sf.Name == null || sf.Id < 0 || sf.Password == null || sf.Dob == null || sf.Title == null)
+            {
+                ViewBag.Error = "Please enter all the values in each field";
+                return View();
+            }
+            else
+            {
+                (from p in StaffContext.Staffobject
+                 where p.Id == sf.Id
+                 select p).ToList().ForEach(x =>
+                 {
+                     x.Password = sf.Password;
+                     x.Dob = sf.Dob;
+                     x.Title = sf.Title;
+                     x.Name = sf.Name;
+                 });
+                return RedirectToAction("StaffList");
+            }
+
+        }
+
+        public IActionResult ViewStaff(int id)
+        {
+            Staff temp = StaffContext.Staffobject.Where(x => x.Id == id).FirstOrDefault();
+            return View(temp);
+        }
+
+        public IActionResult DeleteStaff(int id)
+        {
+            Staff temp = StaffContext.Staffobject.Where(x => x.Id == id).FirstOrDefault();
+            return View(temp);
+        }
+        [HttpPost]
+
+        public IActionResult DeleteStaff(Staff sf)
+        {
+            StaffContext.Staffobject = StaffContext.Staffobject.Where(x => x.Id != sf.Id).ToList();
+            return RedirectToAction("StaffList");
+        }
+    }
+}
---
+++ WebsiteExampleMain/Models/StaffContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace WebsiteExampleMain.Models
+{
+    public class StaffContext
+    {
+        public static List<Staff> Staffobject = new List<Staff>();
+
+        public StaffContext()
+        {
+            if(Staffobject.Count == 0)
+            {
+                Staffobject.Add(new Staff(1, "Josh", "Baby staff member", "1234", "2009"));
+                Staffobject.Add(new Staff(2, "Jessica", "staff member", "4321", "2000"));
+                Staffobject.Add(new Staff(3, "Jebidiya", "dead staff member", "3456", "1879"));
+            }
+
+        } 
+    }
+}
